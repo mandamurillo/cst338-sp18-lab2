b7ac5b9d2fab46586054c9bc044d357241f2f126
MariaEAndaMurillo <manda-murillo@csumb.edu>
2018-02-12 05:29:08 +0000

    changes

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+include 'inc/function.php'
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> 777 Slot Machine </title>
+        <style>
+            @import url("/cst336/lab2/777/css/styles.css");
+        </style>
+    </head>
+    <body>
+    <?php
+    play();
+    ?>
+    <div id="main">
+        <form>
+        <input type="submit" value="Spin!"/>
+    </form>
+</div>
+    
+
+    </body>
+</html>
